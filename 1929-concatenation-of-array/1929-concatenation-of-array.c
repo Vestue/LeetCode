7c970c2db76c61489a9ba1944d2839310d380f69
Ragnar Winblad von Walter <61783376+Vestue@users.noreply.github.com>
2022-07-15 17:10:05 +0200

    Time: 62 ms (22.94%), Space: 9.7 MB (83.17%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.c
@@ -0,0 +1,16 @@
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* getConcatenation(int* nums, int numsSize, int* returnSize){
+    int* arr = (int*)malloc(numsSize * 2 * sizeof(int));
+    if (arr == NULL) return NULL;
+    
+    for (int i = 0; i < numsSize; i++) {
+        arr[i] = nums[i];
+        arr[i + numsSize] = nums[i];
+    }
+    *returnSize = numsSize * 2;
+    return arr;
+}
