8d089e7987c42aafdbcd4157c588a4a0c9958434
Ragnar Winblad von Walter <61783376+Vestue@users.noreply.github.com>
2022-07-15 16:42:44 +0200

    Time: 194 ms (65.68%), Space: 46.9 MB (15.95%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cs
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cs
@@ -1,6 +1,5 @@
 public class Solution {
     public int[] GetConcatenation(int[] nums) {
-        if (nums.Length < 1 || nums.Length > 1000) return default;
         int[] arr = new int[nums.Length * 2];
         nums.CopyTo(arr, 0);
         nums.CopyTo(arr, nums.Length);
