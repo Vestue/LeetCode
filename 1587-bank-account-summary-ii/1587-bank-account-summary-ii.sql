eae235f856888e1c33775ce8b167bf83ba0fbe3f
Ragnar Winblad von Walter <61783376+Vestue@users.noreply.github.com>
2022-07-16 21:38:06 +0200

    Time: 910 ms (33.15%), Space: 0B (100.00%) - LeetHub

--- 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -1,8 +1,3 @@
-# Write your MySQL query statement below
-SELECT
-    name,
-    SUM(amount) balance
-FROM USERS
-    LEFT JOIN TRANSACTIONS
-        USING(account)
+SELECT name, SUM(amount) balance
+FROM USERS LEFT JOIN TRANSACTIONS USING(account)
 GROUP BY account HAVING balance > 10000
