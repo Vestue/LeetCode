0aa710f507f0aa78f03aa1b0410270f53be172a2
Ragnar Winblad von Walter <61783376+Vestue@users.noreply.github.com>
2022-07-16 21:39:28 +0200

    Time: 1445 ms (12.09%), Space: 0B (100.00%) - LeetHub

--- 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -1,3 +1,8 @@
-SELECT name, SUM(amount) balance
-FROM USERS LEFT JOIN TRANSACTIONS USING(account)
-GROUP BY account HAVING balance > 10000
+SELECT 
+    name, 
+    SUM(amount) AS 'balance'
+FROM USERS 
+    LEFT JOIN TRANSACTIONS 
+        USING(account)
+GROUP BY account 
+    HAVING balance > 10000
