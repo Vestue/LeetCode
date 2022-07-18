f179ace1aa5a9c39752c3c073af30990033f3518
Ragnar Winblad von Walter <61783376+Vestue@users.noreply.github.com>
2022-07-18 16:08:50 +0200

    Time: 9 ms (50.88%), Space: 6.5 MB (75.31%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.c
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.c
@@ -4,8 +4,12 @@ int finalValueAfterOperations(char ** operations, int operationsSize){
     int val = 0;
     for(int i = 0; i < operationsSize; i++)
     {
-        if (operations[i][1] == '+') val++;
-        else val--;   
+        if (operations[i][1] == '+'){
+            val++;
+        }
+        else{ 
+            val--;
+        }
     }
     return val;
 }
